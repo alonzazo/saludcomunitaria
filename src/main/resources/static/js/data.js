36d04394bb49b6b84327ded703f812ddc6f25e20
Oscar Azofeifa <oscar.azofeifa.segura@gmail.com>
2018-11-02 14:16:15 -0600

    #HU10 Despliegue de datos interactivo en el mapa

--- src/main/resources/static/js/data.js
+++ src/main/resources/static/js/data.js
@@ -17,4 +17,40 @@ ready(function () {
         center: [-83.67,8.69], // starting position [lng, lat]
         zoom: 14
     });
+
+    map.on('click', function(e){
+        var features = map.queryRenderedFeatures(e.point, {
+            layers: ['boundaries']
+        });
+
+        if (!features.length) {
+            return;
+        }
+
+        var feature = features[0];
+
+        var popup = new mapboxgl.Popup({ offset: [0, -15] , className:'popup-map'})
+            .setLngLat(JSON.parse(feature.properties.coordinates))
+            .setHTML('<h3>' + feature.properties.name + '</h3>' +
+                '<p> Agua: ' + feature.properties.aguas + '</p>' +
+                '<p> Frecuencia: ' + feature.properties.frecuencia + '</p>' +
+                '<p> Porcentaje: ' + feature.properties.porcentaje + '</p>')
+            .setLngLat(JSON.parse(feature.properties.coordinates))
+            .addTo(map);
+    });
+
+    // Center the map on the coordinates of any clicked symbol from the 'symbols' layer.
+    map.on('click', 'boundaries', function (e) {
+        map.flyTo({center: JSON.parse(e.features[0].properties.coordinates)});
+    });
+
+    // Change the cursor to a pointer when the it enters a feature in the 'symbols' layer.
+    map.on('mouseenter', 'boundaries', function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'boundaries', function () {
+        map.getCanvas().style.cursor = '';
+    });
 });
