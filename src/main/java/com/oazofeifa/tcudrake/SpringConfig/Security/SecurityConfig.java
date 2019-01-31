8e9ef76bf48ef63eb31b9f2637504f2e2aa941b6
Oscar Azofeifa <oscar.azofeifa.segura@gmail.com>
2019-01-30 20:01:12 -0600

    Versión de funcional para producción

--- src/main/java/com/oazofeifa/tcudrake/SpringConfig/Security/SecurityConfig.java
+++ src/main/java/com/oazofeifa/tcudrake/SpringConfig/Security/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.oazofeifa.tcudrake.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -13,6 +14,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.keygen.KeyGenerators;
 import org.springframework.security.crypto.keygen.StringKeyGenerator;
+
+import javax.persistence.EntityManagerFactory;
 
 @Configuration
 @EnableWebSecurity
--- src/main/java/com/oazofeifa/tcudrake/TcuDrakeApplication.java
+++ src/main/java/com/oazofeifa/tcudrake/TcuDrakeApplication.java
@@ -1,18 +1,29 @@
 package com.oazofeifa.tcudrake;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.keygen.KeyGenerators;
 import org.springframework.security.crypto.keygen.StringKeyGenerator;
 
 @SpringBootApplication
-public class TcuDrakeApplication {
+/*@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)*/
+public class TcuDrakeApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TcuDrakeApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TcuDrakeApplication.class);
+    }
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
