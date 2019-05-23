bf5071fc6155a6b52a2b2fdfe35aa698ecd4d0f2
Diego Mora <diego.morajimenez@ucr.ac.cr>
2019-05-23 14:35:57 -0600

    Add contact section

--- src/main/java/com/oazofeifa/tcudrake/WebServices/Controllers/ContactController.java
+++ src/main/java/com/oazofeifa/tcudrake/WebServices/Controllers/ContactController.java
@@ -1,4 +1,25 @@
 package com.oazofeifa.tcudrake.WebServices.Controllers;
 
+import com.oazofeifa.tcudrake.Persistence.Entities.BlogEntry;
+import com.oazofeifa.tcudrake.Persistence.Entities.User;
+import com.oazofeifa.tcudrake.Services.BlogEntriesService;
+import com.oazofeifa.tcudrake.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.jws.WebParam;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Controller
 public class ContactController {
+
+    @RequestMapping(value = "/contact")
+    public ModelAndView getContactMainPage(ModelAndView mv){
+        mv.setViewName("contact");
+        return mv;
+    }
 }
